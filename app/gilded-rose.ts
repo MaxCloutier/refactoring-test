f4f80b0db6c263bd5fe1caa9095156c79166ff84
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-10-30 10:26:34 -0700

    Fix return so I can destructure the item object properly

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,8 +19,9 @@ export class GildedRose {
   }
 
   updateQuality() {
-    // Using .map to make it cleaner, I won't need to carry over this.items through the loop and I can just return what .map returns
-    return this.items.map((item) => {
+    // Using .map to make it cleaner and I won't need to carry over this.items through the loop
+    this.items = this.items.map((item) => {
+      let { sellIn, quality } = item
       const { name } = item
       // Sulfuras, Hand of Ragnaros never changes so no need to go any further
       if (name === 'Sulfuras, Hand of Ragnaros') {
@@ -36,18 +37,18 @@ export class GildedRose {
         case 'Backstage passes to a TAFKAL80ETC concert':
           qualityChange = 1
 
-          if (item.sellIn < 11) {
+          if (sellIn < 11) {
             qualityChange = 2
           }
-          if (item.sellIn < 6) {
+          if (sellIn < 6) {
             qualityChange = 3
           }
           break;
       }
 
-      item.sellIn = item.sellIn - 1;
+      sellIn = sellIn - 1
 
-      if (item.sellIn < 0) {
+      if (sellIn < 0) {
         // again, a switch seem more appropriate and easy to read to handle this kind of logic
         switch (name) {
           case 'Aged Brie':
@@ -63,15 +64,17 @@ export class GildedRose {
         }
       }
 
-      item.quality = item.quality + qualityChange
+      quality = quality + qualityChange
 
-      if (item.quality > 50) {
-        item.quality = 50
-      } else if (item.quality < 0) {
-        item.quality = 0
+      if (quality > 50) {
+        quality = 50
+      } else if (quality < 0) {
+        quality = 0
       }
 
-      return item
+      return {name, sellIn, quality}
     })
+
+    return this.items
   }
 }
